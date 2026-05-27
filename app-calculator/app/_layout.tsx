ad110b9c443d2d99cd1a840f822ff3a2da1619a1
JustinS993 <justinsebastian304050@gmail.com>
2026-05-26 19:46:37 -0500

    Agregando estilos globales y aplicándolos al RootLayout

--- app-calculator/app/_layout.tsx
+++ app-calculator/app/_layout.tsx
@@ -2,10 +2,11 @@ import { View } from 'react-native';
 import { Slot } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { globalStyles } from '@/styles/globalStyles';
 
 const RootLayout = () => {
   return (
-    <View>
+    <View style={globalStyles.background}>
       <Slot />
       <StatusBar style="light" />
     </View>
